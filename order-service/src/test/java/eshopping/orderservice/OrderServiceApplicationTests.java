a288a2799929689b1834c095e1001d55b84f80f1
Shrishti34 <saxena.shrishti03@gmail.com>
2021-06-07 18:47:40 +0530

    new

---
+++ order-service/src/test/java/eshopping/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eshopping.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
