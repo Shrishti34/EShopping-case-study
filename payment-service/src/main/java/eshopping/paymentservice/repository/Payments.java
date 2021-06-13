f799da8947d872999f2d87c0b3adfbdf86e73b37
Shrishti34 <saxena.shrishti03@gmail.com>
2021-06-13 15:50:15 +0530

    new updates

---
+++ payment-service/src/main/java/eshopping/paymentservice/repository/Payments.java
@@ -0,0 +1,11 @@
+package eshopping.paymentservice.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import eshopping.paymentservice.domain.Payment;
+
+
+
+public interface Payments extends MongoRepository<Payment, String> {
+
+}
